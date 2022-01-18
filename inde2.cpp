a676593818badc607eb53d77f5b1f3540191af2c
Abdul Razzak <abdulrazzak74832@gmail.com>
2022-01-18 17:56:01 +0530

    cpp files are added

---
+++ inde2.cpp
@@ -0,0 +1,14 @@
+iiiiindbnmf df
+f
+re
+f
+rg
+
+rg
+
+g
+g
+
+gg
+
+gdjhdjsdshde
---
+++ index.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=4;
+	int c,b=5;
+	cout<<c=b+a<<endl;
+	cout<<"Hello everyone i m learning and practicing git and github "<<endl;
+	cout<<"After this im start solving begining dsa sheet "<<endl;
+	return 0;
+
+}
